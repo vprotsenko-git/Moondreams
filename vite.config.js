c69b2ccdb56052da00c1d90df3dabbba1ae552f2
vprotsenko-git <vasiliyprotsenko.net@gmail.com>
2025-07-28 04:23:46 +0300

    Update vite.config.js remove port

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: 'public',      // будемо брати index.html з public/
+  build: {
+    outDir: '../dist', // зберігатиметься ../dist
+    emptyOutDir: true,
+  },
   plugins: [react()],
-  server: {
-    port: 3000
-  }
 })
