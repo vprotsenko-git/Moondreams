d6d451e365fd41493f736b444c9e9d4a4ab4c60b
vprotsenko-git <vasiliyprotsenko.net@gmail.com>
2025-07-29 16:26:01 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -4,10 +4,4 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000'
 });
 
-API.interceptors.request.use(cfg => {
-  const token = localStorage.getItem('moondreams_token');
-  if (token) cfg.headers.Authorization = `Bearer ${token}`;
-  return cfg;
-});
-
 export default API;
