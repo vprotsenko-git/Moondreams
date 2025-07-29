74ccbd5fb18e906ec25d89e59c9bfb7ce006f52c
vprotsenko-git <vasiliyprotsenko.net@gmail.com>
2025-07-29 16:30:31 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,29 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import { BrowserRouter as Router, Route, Routes, Navigate, Link } from 'react-router-dom';
 import Text2Img from './pages/Text2Img';
 import Img2Video from './pages/Img2Video';
-import Generate from './pages/Generate';
-import Upscale from './pages/Upscale';
-import TrainModel from './pages/TrainModel';
-import ProtectedRoute from './components/ProtectedRoute';
-import './App.css'; // базові стилі
+import Login from './pages/Login';
+import Register from './pages/Register';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
-      <div className="app-container">
-        <Sidebar />
-        <div className="main-content">
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/generate" element={<Generate />} />
-            <Route path="/register" element={<Register />} />
+    <Router>
+      <nav className="navbar">
+        <Link to="/">Text2Img</Link>
+        <Link to="/img2video">Img2Video</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </nav>
 
-            <Route
-              path="/*"
-              element={
-                <ProtectedRoute>
-                  <Routes>
-                    <Route path="text2img" element={<Text2Img />} />
-                    <Route path="img2video" element={<Img2Video />} />
-                    <Route path="upscale" element={<Upscale />} />
-                    <Route path="train" element={<TrainModel />} />
-                    <Route path="assets" element={<div>Assets list</div>} />
-                    <Route path="*" element={<Navigate to="text2img" />} />
-                  </Routes>
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Text2Img />} />
+        <Route path="/img2video" element={<Img2Video />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
+
+export default App;
